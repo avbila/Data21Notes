0a4a58f96dfaf1dd2ff1d16c868c6e4dd6d446b0
avbila <abilla@spartaglobal.com>
2021-04-22 13:27:59 +0100

    Second check point

---
+++ advent_calender_2020.py
@@ -0,0 +1,28 @@
+def day_15():
+    numbers_dictionary = {}
+    #starting_string = [1,3,2]
+    starting_string = [6,4,12,1,20,0,16]
+    # nums = starting_string
+
+    for i in range(len(starting_string)-1):
+        numbers_dictionary[starting_string[i]] = i+1
+    prev_num = starting_string[-1]
+    index = len(starting_string)+1
+    stopping_index = 30000000
+
+    while index <= stopping_index:
+
+        # Numbers not previously used:
+        if prev_num not in numbers_dictionary.keys():
+            cur_num = 0
+        # Numbers previously used:
+        else:
+            cur_num = index - 1 - numbers_dictionary[prev_num]
+
+        numbers_dictionary[prev_num] = index - 1
+        prev_num = cur_num
+        # nums.append(cur_num)
+        index += 1
+
+    print(cur_num)
+
