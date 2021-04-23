02931c81041b579e65521b1afc5ee56ff310aa32
avbila <abilla@spartaglobal.com>
2021-04-23 09:23:56 +0100

    22/4/2021 2nd checkpoint

--- test.py
+++ test.py
@@ -1,26 +1,4 @@
-# list_num=[]
-# for x in range(1, 1000):
-#     if x % 3 == 0:
-#         list_num.append(x)
-#     elif x % 5 == 0:
-#         list_num.append(x)
-# print(list_num)
-# y = sum(list_num)
-# print(y) #233168
+import math as m
 
-# # problem 2:
-# numbers_list = [1, 2]
-# sum_of_numbers = 2
-# while True:
-#     current = sum(numbers_list)
-#     numbers_list[0] = numbers_list[1]
-#     numbers_list[1] = current
-#
-#     if current > 4000000:
-#         break
-#     else:
-#         if current % 2 == 0:
-#             sum_of_numbers += current
-# print(sum_of_numbers) #4613732
 
-#problem 3:
+print(m.lcm(1,2,3,4,5,6,7,8,9,10))
